2450b1dd3a793840776d193c31e85f9d6c2308d2
章文俊 <1346502061@qq.com>
2023-06-06 09:18:01 +0800

    Config

--- src/Config.ts
+++ src/Config.ts
@@ -1,13 +1,7 @@
-import { appWindow } from "@tauri-apps/api/window"
-
 function Tauri() {
     window.addEventListener('contextmenu', (e) => {
         e.stopPropagation()
         e.preventDefault()
-    })
-    appWindow.once('tauri://window-created', async () => {
-        await appWindow.show()
-        await appWindow.setFocus()
     })
 }
 
