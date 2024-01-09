6fc336e22133485cad4d0cf2f7215481a18c8a03
zhangwenjun <1346502061@qq.com>
2024-01-09 12:16:19 +0800

    Update:Format

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script lang="ts" setup>
 import PreloadVue from './preload/Preload.vue';
-import ExperimentVue from './experiment/Experiment.vue';
 import { zhCN, dateZhCN } from 'naive-ui'
 import { themeOverrides } from './naive/index'
 import { App } from './App'
@@ -16,8 +15,10 @@ App.Instance.Run()
     <n-config-provider :locale="zhCN" :date-locale="dateZhCN" :theme-overrides="themeOverrides">
         <n-message-provider>
             <n-dialog-provider>
-                <PreloadVue></PreloadVue>
-                <router-view />
+                <n-notification-provider placement="bottom-right">
+                    <PreloadVue></PreloadVue>
+                    <router-view />
+                </n-notification-provider>
             </n-dialog-provider>
         </n-message-provider>
     </n-config-provider>
