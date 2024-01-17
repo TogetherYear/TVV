5ee72cd7a11beee968ae994cc8621c3ff5e1f756
zhangwenjun <1346502061@qq.com>
2024-01-17 16:10:34 +0800

    Update:Config

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -11,6 +11,85 @@ use window_shadows::set_shadow;
 
 use windows_sys::Win32::{Foundation::POINT, UI::WindowsAndMessaging::GetCursorPos};
 
+fn main() {
+    Builder::default()
+        .setup(|app| {
+            Ok({
+                OnSetup(app);
+            })
+        })
+        .plugin(tauri_plugin_single_instance::init(|app, _argv, _cwd| {
+            OnSecondInstance(app);
+        }))
+        .invoke_handler(generate_handler![GetRustStruct])
+        .system_tray(SystemTray::new().with_tooltip("去码头整点薯条"))
+        .on_system_tray_event(|app, event| {
+            OnTrayEvent(app, event);
+        })
+        .run(generate_context!())
+        .expect("Error while running tauri application");
+}
+
+fn OnSetup(app: &mut tauri::App) {
+    let shadowWindow = app.get_window("Application").unwrap();
+    set_shadow(&shadowWindow, true).expect("Unsupported platform!");
+}
+
+fn OnSecondInstance(app: &tauri::AppHandle) {
+    let window = app.get_window("Application").unwrap();
+    if window.is_minimized().unwrap() {
+        window.unminimize().unwrap();
+    } else {
+        window.show().unwrap();
+    }
+    window.set_focus().unwrap();
+}
+
+fn OnTrayEvent(app: &tauri::AppHandle, event: SystemTrayEvent) {
+    match event {
+        SystemTrayEvent::DoubleClick {
+            position: _,
+            size: _,
+            ..
+        } => {
+            let window = app.get_window("Application").unwrap();
+            if window.is_minimized().unwrap() {
+                window.unminimize().unwrap();
+            } else {
+                window.show().unwrap();
+            }
+            window.set_focus().unwrap();
+        }
+        SystemTrayEvent::RightClick {
+            position: _,
+            size: _,
+            ..
+        } => {
+            let window = app.get_window("Tray").unwrap();
+            let point = GetCursorPosition();
+            let size = window.inner_size().unwrap();
+            window
+                .set_position(PhysicalPosition::new(
+                    (point.x as u32) - size.width + 2,
+                    (point.y as u32) - size.height + 2,
+                ))
+                .unwrap();
+            window.set_always_on_top(true).unwrap();
+            window.show().unwrap();
+            window.set_focus().unwrap();
+        }
+        _ => {}
+    }
+}
+
+fn GetCursorPosition() -> POINT {
+    let mut pt = POINT { x: -1, y: -1 };
+    unsafe {
+        GetCursorPos(&mut pt);
+    };
+    pt
+}
+
 #[derive(Clone, serde::Serialize)]
 struct Rect {
     left: i32,
@@ -27,68 +106,4 @@ fn GetRustStruct() -> Rect {
         top: -1,
         bottom: -1,
     }
-}
-
-fn GetCursorPosition() -> POINT {
-    let mut pt = POINT { x: -1, y: -1 };
-    unsafe {
-        GetCursorPos(&mut pt);
-    };
-    pt
-}
-
-fn main() {
-    Builder::default()
-        .setup(|app| {
-            let windowA = app.get_window("Application").unwrap();
-            set_shadow(&windowA, true).expect("Unsupported platform!");
-            Ok(())
-        })
-        .plugin(tauri_plugin_single_instance::init(|app, _argv, _cwd| {
-            let window = app.get_window("Application").unwrap();
-            if window.is_minimized().unwrap() {
-                window.unminimize().unwrap();
-            } else {
-                window.show().unwrap();
-            }
-            window.set_focus().unwrap();
-        }))
-        .invoke_handler(generate_handler![GetRustStruct,])
-        .system_tray(SystemTray::new().with_tooltip("去码头整点薯条"))
-        .on_system_tray_event(|app, event| match event {
-            SystemTrayEvent::DoubleClick {
-                position: _,
-                size: _,
-                ..
-            } => {
-                let window = app.get_window("Application").unwrap();
-                if window.is_minimized().unwrap() {
-                    window.unminimize().unwrap();
-                } else {
-                    window.show().unwrap();
-                }
-                window.set_focus().unwrap();
-            }
-            SystemTrayEvent::RightClick {
-                position: _,
-                size: _,
-                ..
-            } => {
-                let window = app.get_window("Tray").unwrap();
-                let point = GetCursorPosition();
-                let size = window.inner_size().unwrap();
-                window
-                    .set_position(PhysicalPosition::new(
-                        (point.x as u32) - size.width + 2,
-                        (point.y as u32) - size.height + 2,
-                    ))
-                    .unwrap();
-                window.set_always_on_top(true).unwrap();
-                window.show().unwrap();
-                window.set_focus().unwrap();
-            }
-            _ => {}
-        })
-        .run(generate_context!())
-        .expect("Error while running tauri application");
 }
