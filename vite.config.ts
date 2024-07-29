e44dea51a26fd4b07022e5449ce802919834613e
TSingleton <togetheryear@gmail.com>
2024-07-29 14:11:15 +0800

    Update:Config

--- vite.config.ts
+++ vite.config.ts
@@ -3,42 +3,44 @@ import vue from '@vitejs/plugin-vue';
 
 import path from 'path';
 
-export default defineConfig({
-    clearScreen: false,
-    plugins: [
-        vue({
-            script: {
-                defineModel: true
+export default defineConfig(({ command, mode }) => {
+    return {
+        clearScreen: false,
+        plugins: [
+            vue({
+                script: {
+                    defineModel: true
+                }
+            })
+        ],
+        resolve: {
+            alias: {
+                '@': path.resolve('Src')
             }
-        })
-    ],
-    resolve: {
-        alias: {
-            '@': path.resolve('Src')
-        }
-    },
-    build: {
-        target: ['esnext'],
-        minify: 'esbuild',
-        sourcemap: false,
-        emptyOutDir: true,
-        assetsDir: 'Source',
-        rollupOptions: {
-            output: {
-                manualChunks: (id: string) => {
-                    if (id.includes('node_modules')) {
-                        return 'Vendor';
+        },
+        build: {
+            target: ['esnext'],
+            minify: 'esbuild',
+            sourcemap: false,
+            emptyOutDir: true,
+            assetsDir: 'Source',
+            rollupOptions: {
+                output: {
+                    manualChunks: (id: string) => {
+                        if (id.includes('node_modules')) {
+                            return 'Vendor';
+                        }
                     }
                 }
             }
+        },
+        base: './',
+        envDir: './Env',
+        root: path.join(__dirname, ''),
+        publicDir: 'Public',
+        server: {
+            port: 6768,
+            strictPort: true
         }
-    },
-    base: './',
-    envDir: './Env',
-    root: path.join(__dirname, ''),
-    publicDir: 'Public',
-    server: {
-        port: 6768,
-        strictPort: true
-    }
+    };
 });
