d90d152631c2370677a760f5998715ea9b9568d1
TSingleton <togetheryear@gmail.com>
2024-07-31 10:07:09 +0800

    Update:去除组件库

--- Src/main.ts
+++ Src/main.ts
@@ -4,9 +4,7 @@ import AppVue from './App.vue';
 
 import router from './Router';
 
-import { naive } from './Naive';
-
 import { Renderer } from './Plugins/Renderer';
 await Renderer.Run();
 
-createApp(AppVue).use(router).use(naive).mount('#App');
+createApp(AppVue).use(router).mount('#App');
