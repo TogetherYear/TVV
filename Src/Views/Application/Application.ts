f0767d2005013d77548e90c7cee5f14bfdcee7c4
TSingleton <togetheryear@gmail.com>
2024-09-09 14:15:09 +0800

    Update:TRouter

--- Src/Views/Application/Application.ts
+++ Src/Views/Application/Application.ts
@@ -1,12 +1,7 @@
 import { onMounted, onUnmounted } from 'vue';
 import { Component } from '@/Libs/Component';
-import { TRouter } from '@/Decorators/TRouter';
 import { Renderer } from '@/Plugins/Renderer';
 
-@TRouter.View({
-    module: TRouter.Module.Default,
-    duty: TRouter.Duty.Application
-})
 class Application extends Component {
     public constructor() {
         super();
