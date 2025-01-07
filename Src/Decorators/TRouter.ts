291817bf048a2bc7284d2eb00ca7f62f98aa6f1c
章文俊 <1346502061@qq.com>
2025-01-07 09:09:37 +0800

    Update:更新v1

--- Src/Decorators/TRouter.ts
+++ Src/Decorators/TRouter.ts
@@ -196,8 +196,6 @@ namespace TRouter {
                 private TRouter_Generate_Loading() {
                     if (!isLoad) {
                         isLoad = true;
-                        //@ts-ignore
-                        window.HideLoading();
                     }
                 }
             };
