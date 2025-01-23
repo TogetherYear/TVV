e48b041fc34d0e02245ee694fa6c3fe7263c59eb
章文俊 <1346502061@qq.com>
2025-01-23 13:47:30 +0800

    Update:Component

--- src-tauri/src/Extra/Setup.rs
+++ src-tauri/src/Extra/Setup.rs
@@ -1,5 +1,6 @@
 use port_check::is_local_ipv4_port_free;
-use tauri::{api::http::HttpRequestBuilder, App, Manager};
+use reqwest::Client;
+use tauri::{App, Manager};
 
 use super::Serve::{self, PORT};
 
@@ -13,9 +14,7 @@ pub fn IsExistApp() -> bool {
 }
 
 pub async fn FocusFirstApp() {
-    let client = tauri::api::http::ClientBuilder::new().build().unwrap();
-    let request =
-        HttpRequestBuilder::new("GET", format!("http://localhost:{}/secondinstance", PORT))
-            .unwrap();
-    let _ = client.send(request).await;
+    let client = Client::new();
+    let url = format!("http://localhost:{}/secondinstance", PORT);
+    let _ = client.get(&url).send().await;
 }
